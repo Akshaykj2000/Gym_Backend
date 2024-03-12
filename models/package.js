953105b74af40c37e9e4e35252b160f8eb389614
Annateresa01 <shajianna013@gmail.com>
2024-03-12 23:08:21 +0530

    ok

--- models/package.js
+++ models/package.js
@@ -11,10 +11,16 @@ const packageModel={
     insertpackage:(packagedata,callback)=>{
     const query='INSERT INTO packages SET ?';
     pool.query(query,packagedata,callback)
-},
+    },
+
     viewPackage:(callback)=>{
         const query='SELECT * FROM packages'
         pool.query(query,callback)
+    },
+    
+    deletePackage:(id,callback)=>{
+        const query='DELETE FROM packages WHERE id=?';
+        pool.query(query,[id],callback)
     }
 }
 module.exports=packageModel;
